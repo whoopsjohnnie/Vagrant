aa4194d3c90de2f28db137ff024d6ce87781d484
John Grundback <john@Johns-MacBook-Pro.local>
2022-09-04 00:50:52 -0700

    Found old tests of Ubuntu 20.04

--- vms/ubuntu-20.04-focal64/bootstrap.sh
+++ vms/ubuntu-20.04-focal64/bootstrap.sh
@@ -1,4 +1,4 @@
-
+b
 # !/bin/sh
 
 apt install -y virtualbox-guest-additions-iso
@@ -14,9 +14,19 @@ apt install -y ubuntu-desktop
 # apt install -y xinit
 # apt install -y ubuntu-unity-desktop
 
+# Window Maker
+apt install -y x-window-system-core wmaker menu
+
 # GNUStep
-# apt install -y x-window-system-core wmaker menu
 # apt install -y gnustep
+
+# 
+apt install -y nano
+apt install -y mg
+apt install -y mc
+
+# link emacs
+ln -s /usr/bin/mg /usr/bin/emacs
 
 # 
 apt install -y zsh
